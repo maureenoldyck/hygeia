43595bff84c13a9d8c397fb4159ceccb829458ef
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-18 11:07:32 +0100

    Changing icon stroke width

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
+import feed from '../assets/feed.svg';
+import chatroom from '../assets/chatroom.svg';
+import forums from '../assets/forums.svg';
+
+import notifications from '../assets/notifications-none.svg';
+import messages from '../assets/messages-none.svg';
+
+import search from '../assets/search.svg'
+
 
 const Header = () => {
     return (
@@ -18,18 +27,18 @@ const Header = () => {
 
                     <div className="flex flex-row" id="search">
                         <input type="search" placeholder="Search.." />
-                        <button>Go!</button>
+                        <img src={search} alt="search" className="h-8 w-8"/>
                     </div>
 
-                    <ul className="flex flex-row w-full pl-60 justify-around mx-34">
-                        <li><a href="/profile">Feed</a></li>
-                        <li><a href="/profile">Forums</a></li>
-                        <li><a href="/profile">Chatrooms</a></li>
+                    <ul className="flex flex-row w-full pl-60 justify-around mx-34 z-50">
+                        <li><a href="/profile"><img src={feed} alt="Feed" className="h-8 w-8"/></a></li>
+                        <li><a href="/profile"><img src={forums} alt="forums" className="h-8 w-8"/></a></li>
+                        <li><a href="/profile"><img src={chatroom} alt="chatroom" className="h-8 w-8"/></a></li>
                     </ul>
 
                     <ul className="flex flex-row w-full justify-around mx-36">
-                        <li><a href="/profile">Nots</a></li>
-                        <li><a href="/profile">Mess</a></li>
+                        <li><a href="/profile"><img src={notifications} alt="notifications" className="h-8 w-8"/></a></li>
+                        <li><a href="/profile"><img src={messages} alt="messages" className="h-8 w-8"/></a></li>
                     </ul>
 
                 </nav>
