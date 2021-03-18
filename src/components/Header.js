c1c9169b2446a182f58bc92e8784b13896fa1269
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-18 10:45:45 +0100

    Adding open source icons to assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,38 @@ import logo from '../assets/logo.svg';
 const Header = () => {
     return (
         
-        <div className="absolute h-auto z-50 flex flex-row w-full">
-            <div className="flex flex-row pl-4 pt-6 transform scale-50" id="logo">
-                <a href="/" className="flex flex-row">
+        <div className="absolute h-auto bg-green-100 z-50 flex flex-row p-4 w-full">
+            <div className="flex flex-row" id="logo">
+                <a href="/" className="flex flex-row h-10">
                     <img src={logo} alt="Logo"/>
-                    <h1 className="flex items-end text-5xl py-4">ygeia</h1>
+                    <h1 className="flex py-1 text-2xl">ygeia</h1>
                 </a>
             </div>
+
             <div className="flex flex-row w-full justify-between">
-                <nav className="w-full">
-                    <ul className="flex flex-row justify-around">
-                        <li><a href="/profile">Searchbar</a></li>
+
+                <nav className="flex flex-row w-full">
+
+                    <div className="flex flex-row" id="search">
+                        <input type="search" placeholder="Search.." />
+                        <button>Go!</button>
+                    </div>
+
+                    <ul className="flex flex-row w-full pl-60 justify-around mx-34">
                         <li><a href="/profile">Feed</a></li>
                         <li><a href="/profile">Forums</a></li>
                         <li><a href="/profile">Chatrooms</a></li>
-                        <li><a href="/profile">Profile</a></li>
+                    </ul>
+
+                    <ul className="flex flex-row w-full justify-around mx-36">
                         <li><a href="/profile">Nots</a></li>
                         <li><a href="/profile">Mess</a></li>
                     </ul>
+
                 </nav>
 
                 <div id="logout">
+                    <a href="/profile">Profile</a>
                     <a href="/">Logout</a>
                 </div>
             </div>
