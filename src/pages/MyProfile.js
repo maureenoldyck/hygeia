207415a9bf33fdd167ba80ac940c31e793e57d88
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-03-19 09:44:57 +0100

    Add details component information

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -3,6 +3,8 @@ import Profile from '../components/Profile.js';
 import Profile_form from '../components/Profile_form.js';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
+import Details from '../components/Details.js';
+import DetailsForm from '../components/Details_form.js';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -22,15 +24,20 @@ const MyProfile = () => {
                         <div className="flex justify-around items-center flex-col w-full">
                             <div className="bg-green-profilebg bg-opacity-25 w-nine h-2/5 my-6" id="profile">
                                 <Router>
-                                    <Switch>
-                                        <Route path="/profile" exact component={Profile} />
-                                        <Route path="/profile/edit/profile" exact component={Profile_form}/>
-                                    </Switch>
+                                <Switch>
+                                    <Route path="/profile" exact component={Profile} />
+                                    <Route path="/profile/edit/profile" exact component={Profile_form}/>
+                                </Switch>
                                 </Router>
                             </div>
         
                             <div className="bg-green-profiledeets w-nine h-3/5 my-6" id="details">
-
+                                <Router>
+                                <Switch>
+                                    <Route path="/profile" exact component={Details} />
+                                    <Route path="/profile/edit/details" exact component={DetailsForm}/>
+                                </Switch>
+                                </Router>
                             </div>
                         </div>
 
