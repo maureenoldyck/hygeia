24f07f7e4d9d46e6aa72c90ff23f175f08e03263
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-03-21 21:51:33 +0100

    Add overflow scroll to settings profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -19,10 +19,10 @@ const MyProfile = () => {
                 <Header />
             </div>
 
-            <div className="flex justify-around bg-yellow-banana flex-col">
+            <div className="flex justify-around bg-yellow-banana flex-col min-h-full ">
 
-                <div className="flex justify-around flex-row min-h-screen z-0 mb-32 mt-28 px-12" >
-                    <section className="bg-green-profilebg bg-opacity-25 justify-around w-screen min-h-full h-auto flex flex-row" id="left">
+                <div className="flex justify-around flex-row h-screen z-0 mb-32 mt-28 px-12" >
+                    <section className="bg-green-profilebg bg-opacity-25 justify-around w-full h-screen flex flex-row" id="left">
 
                         <div className="flex justify-around items-center flex-col w-full">
                             <div className="bg-green-profilebg bg-opacity-25 w-nine h-2/5 my-6" id="profile">
@@ -50,7 +50,7 @@ const MyProfile = () => {
                         </div>
 
                         <div className="flex justify-around items-center w-full my-6">
-                            <div className="bg-green-profiledeets w-nine min-h-full h-auto " id="settings">
+                            <div className="bg-green-profiledeets w-nine h-full overflow-scroll" id="settings">
                             <Router>
                                     <Switch>
                                         <Route path="/profile/edit/profile" exact component={Settings} />
