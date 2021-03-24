3a5b4072ee82339bf5b75cc705934b2bf2326e26
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-24 14:39:23 +0100

    Committing

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3001");
+    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
