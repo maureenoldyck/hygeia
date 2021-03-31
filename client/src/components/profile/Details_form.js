25b6b593e9ac9a89e78d201667448fe7c977935a
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-03-31 18:05:52 +0200

    Fix form details - When not changing one value, keep the previous value

--- client/src/components/profile/Details_form.js
+++ client/src/components/profile/Details_form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 
@@ -22,7 +22,25 @@ const DetailsForm = () => {
     //==========================================================================================//
     //                                 HandleSubmit: Button event                               //
     //==========================================================================================//
-
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/profile/${id}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": 'application/json,  charset=UTF-8', 
+                'Accept': 'application/json, text/html',
+            },
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then((res) => { 
+            if (age === "") {setAge(res[0].age)};
+            if (gender === "") {setGender(res[0].gender)};
+            if (languages === "") {setLanguages(res[0].language)};
+            if (experiences === "") {setExperiences(res[0].experience_id)};
+            if (website === "") {setWebsite(res[0].my_web)};
+            if (social === "") {setSocial(res[0].my_soc)};
+        });
+    })
 
     const handleDetailsSubmit = () => {
 
@@ -54,7 +72,7 @@ const DetailsForm = () => {
 
     return (
         <div className="w-full h-full lg:text-xl " id="detailsCard">
-            <form method="" className="w-full h-full flex flex-col">
+
                 <div className="flex flex-row justify-between pt-2 mr-6 ml-4 mb-2">
                     <h2 className="lg:text-3xl text-xl">Details</h2>
                     <button className="bg-green-vrt hover:bg-green-vrtdark text-white lg:w-18 lg:h-12 py-2 px-4 rounded-full" onClick={handleDetailsSubmit}><Link to={`/profile/${id}`}>Save</Link></button>
@@ -70,14 +88,14 @@ const DetailsForm = () => {
                             </ul>
                         </div>
                         <div className="bg-gray-input bg-opacity-25 w-3/5 p-2 rounded-md">
-                            <select name="" className="bg-transparent mt-1 block w-full border border-black rounded-md" onChange={(e) => {setAge(e.target.value)}}>
+                            <select name="" className="bg-transparent mt-1 block w-full border border-black rounded-md" placeholder={age} onChange={(e) => {setAge(e.target.value)}}>
                                 <option value="12-20">12-20 y/o</option>
                                 <option value="21-30">21-30 y/o</option>
                                 <option value="31-40">31-40 y/o</option>
                                 <option value="50+">50+ y/o</option>
                             </select>
-                            <input name="" type="text" className="bg-transparent mt-1 block w-full rounded-md" placeholder="Your Gender here" onChange={(e) => {setGender(e.target.value)}}/>
-                            <input name="" type="text" className="bg-transparent mt-1 block w-full rounded-md" placeholder="Your languages here" onChange={(e) => {setLanguages(e.target.value)}}/>
+                            <input name="" type="text" className="bg-transparent mt-1 block w-full rounded-md" placeholder={gender} onChange={(e) => {setGender(e.target.value)}}/>
+                            <input name="" type="text" className="bg-transparent mt-1 block w-full rounded-md" placeholder={languages} onChange={(e) => {setLanguages(e.target.value)}}/>
                         </div>
                     </div>
                     <div className="flex flex-row justify-between pt-2 pb-4 mx-6">
@@ -90,14 +108,14 @@ const DetailsForm = () => {
                         </div>
                         <div className="bg-gray-input bg-opacity-25 w-3/5 my-1 lg:p-4 p-2 rounded-md">
                             
-                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder="Your experiences here" onChange={(e) => {setExperiences(e.target.value)}}/>
-                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder="Your website here" onChange={(e) => {setWebsite(e.target.value)}}/>
-                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder="Your social here" onChange={(e) => {setSocial(e.target.value)}}/>
+                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder={experiences} onChange={(e) => {setExperiences(e.target.value)}}/>
+                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder={website} onChange={(e) => {setWebsite(e.target.value)}}/>
+                            <input name="" type="text" className="bg-transparent mt-0.5 block w-full rounded-md" placeholder={social} onChange={(e) => {setSocial(e.target.value)}}/>
                             
                         </div>
                     </div>
                 </div>
-            </form>
+          
         </div>
     )
 }
