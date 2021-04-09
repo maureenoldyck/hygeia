6b8e3e270df083768846967a271fcca5550b5cd5
WillemDT369 <detroyerwillem@hotmail.com>
2021-04-09 15:09:42 +0200

    Update procfile

--- server/app.js
+++ server/app.js
@@ -90,12 +90,12 @@ app.use((req, res, next) => {
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     //
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname = 'client/build/index.html'));
     })
   }
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/public/index.html'));
   });
 
