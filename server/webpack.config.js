305f562927fb5ff9314403b51a5317f2bc2d9322
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-04-14 12:41:34 +0200

    Deploy WIP

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require( 'path' );
 module.exports = {
+    mode: "production",
     context: __dirname,
-    entry: './src/index.js',
+    entry: './app.js',
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js',
+        filename: 'index.js',
     },
     module: {
         rules: [
