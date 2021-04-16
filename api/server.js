6cbb20b9aa2ac1c373b53f71f06512becc089dad
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-04-16 15:01:11 +0200

    Add header credentials

--- api/server.js
+++ api/server.js
@@ -35,6 +35,7 @@ app.use(cors({
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://hygeia.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
     next();
   });
 
